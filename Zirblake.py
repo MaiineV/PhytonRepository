0faeb60dcb51d53c085d35f2caeb8b42a585dd73
Zirblake <christian-211@live.com>
2022-01-17 00:22:14 -0300

    zirblake update

---
+++ Zirblake.py
@@ -0,0 +1,2 @@
+# Variable = a container for a value. Behaves as the value that it contains, basicamente es un contenedor para un valor, puede ser para valores, numeros, y
+# "boleeans" lo cuales pueden ser "true or false"
